8d5a7dc2dd066c9be41d344d4e2cfb6e51dda434
Jiayi Li <emily1998ljy@gmail.com>
2019-08-18 22:36:50 -0700

    Add files via upload

---
+++ bootstrapt.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    console.log("I am redy");
+    $('.collapse').on('shown.bs.collapse', function(){
+        $(this).parent().find('.glyphicon-menu-down').removeClass('glyphicon-menu-down').addClass('glyphicon-menu-up')
+    }).on('hidden.bs.collapse', function(){
+        $(this).parent().find('.glyphicon-menu-up').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down')
+    })
+    
+    $('img').on('click', function(){
+            
+        $("#modal-img").attr('src', $(this).attr('src'));
+        $("#Modal").modal('show');
+    });
+    
+    
+});
